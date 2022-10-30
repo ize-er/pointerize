8c2d1ba43ab873a60a91abdacc32006eaa541497
Fatur <46602247+FFatur@users.noreply.github.com>
2022-10-30 09:12:45 +0900

    test: add tests

--- test/pointerize.test.ts
+++ test/pointerize.test.ts
@@ -1,7 +1,8 @@
-// import Pointerize from '../lib/pointerize'
+import Pointerize from '../lib/pointerize'
 import { jest } from '@jest/globals'
-// import type { IOptions } from '../lib/types'
+import type { IOptions } from '../lib/types'
 
+//TODO: instance 1 and 2 need more refactoring and tests
 // matchMedia doesn't exist in jsdom so we define it with jest
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -17,230 +18,241 @@ Object.defineProperty(window, 'matchMedia', {
 
 describe('Pointerize options', () => {
   /* 
-    provide as much options to class instance as possible, and test them all.
-    then, with a difference instance, provide the different values that options may have and test the changed values only.
-    repeat ...
+    Specify options on an instance and test. specify the remaining options on another instance and so on ...
   */
 
-  document.body.innerHTML = `<div class="pointerize_root">
+  document.body.innerHTML = `
+     <div class="pointerize_root">
        <div class="interaction_target">
          <div></div>
        </div> 
      </div>`
-  // TODO: needs a big refactor, it's not correct
-  test('temporary empty test', () => {
-    expect(true).toBe(true)
+
+  // instance 0
+  const options_0: IOptions = {} // defaults
+  describe('instance 0', () => {
+    const pointerize = new Pointerize(options_0)
+    pointerize.start()
+
+    test('general things', () => {
+      expect(pointerize.id).toBe('-_pointerize__container_0th')
+      expect(pointerize.element__root?.tagName).toBe('BODY')
+      expect(pointerize.element__svg_container?.tagName).toBe('DIV')
+      expect(pointerize.element__svg?.tagName).toBe('svg')
+    })
   })
-  // // instance 0
-  // const options_0: IOptions = {} // defaults
-  // describe('instance 0', () => {
-  //   const pointerize = new Pointerize(options_0)
-  //   pointerize.start()
 
-  //   test('general things', () => {
-  //     expect(pointerize.element__svg_container).toBeInstanceOf(HTMLElement)
-  //     expect(pointerize.element__svg_container?.tagName).toBe('DIV')
-  //     expect(pointerize.element__svg_container?.firstElementChild?.tagName).toBe('svg')
-  //   })
-  // })
+  // instance 1
+  const options_1: IOptions = {
+    css_selector__root: 'body',
+    element__svg: {
+      svg_attributes: {
+        width: '222',
+      },
+      css_properties: {
+        height: '222px',
+      },
+    },
+    element__svg_container: {
+      css_properties: {
+        width: '333px',
+        left: '10px',
+        top: '100px',
+      },
+    },
+    interactions: [
+      {
+        type: 'pointer',
+        options: {
+          css_selector__root: '.pointerize_root',
+          start_criteria: {
+            criteria: '(pointer: fine)',
+            frequency: 'once',
+          },
+        },
+      },
+      {
+        type: 'pointer__scale',
+        options: {
+          elements: ['.interaction_target'],
+        },
+      },
+    ],
+    // not using `size` because the dimensions stated above need to be tested here
+    shapes: [
+      // 7 basic shapes
+      {
+        type: 'circle',
+        guides: [
+          {
+            type: 'position',
+            options: {
+              shapes: [
+                {
+                  type: 'rectangle'
+                }
+              ]
+            }
+          },
+          {
+            type: 'pattern',
+            options: {
+              preset: 'circle',
+              area: 'fill',
+              ratios: {
+                tile: 0.1,
+                gap: {
+                  row: 0.2,
+                  column: 0.3,
+                },
+              },
+            },
+          },
+        ],
+        ratios: [
+          {
+            type: 'size',
+            options: {
+              value: 0.6
+            }
+          }
+        ],
+        svg_attributes: {
+          'stroke-width': '2',
+          stroke: 'red',
+          // not using `fill` because a pattern is assigned to it
+        },
+        animations: [
+          {
+            preset: 'rotate',
+            css_properties: {
+              'animation-direction': 'reverse',
+            },
+          },
+        ],
+        effects: [
+          {
+            preset: 'glow',
+          },
+        ],
+      },
+      {
+        type: 'ellipse',
+        ratios: [
+          {
+            type: 'radius',
+            options: {
+              value: 0.4,
+            },
+          },
+          {
+            type: 'size',
+            options: {
+              value: 0.8
+            }
+          }
+        ],
+        animations: [
+          {
+            preset: 'rotate',
+          },
+        ],
+        effects: [
+          {
+            preset: 'glow',
+          },
+        ]
+      },
+      {
+        type: 'rectangle',
+        svg_attributes: {
+          width: '10',
+          height: '18',
+          transform: 'skewY(10)',
+        },
+      },
+      {
+        type: 'polygon',
+        sides: 6,
+        ratios: [
+          {
+            type: 'radius',
+            options: {
+              type: 'accumulate',
+              value: 0.4,
+            },
+          },
+        ],
+      },
+      {
+        type: 'line',
+      },
+      {
+        type: 'polyline',
+      },
+      {
+        type: 'path',
+      },
+    ],
+  }
+  describe('instance 1', () => {
+    const pointerize = new Pointerize(options_1)
+    pointerize.start()
 
-  // // instance 1
-  // const options_1: IOptions = {
-  //   css_selector__root: 'body',
-  //   element__svg: {
-  //     svg_attributes: {
-  //       width: '222',
-  //     },
-  //     css_properties: {
-  //       height: '222px',
-  //     },
-  //   },
-  //   element__svg_container: {
-  //     css_properties: {
-  //       width: '333px',
-  //       left: '10px',
-  //       top: '100px',
-  //     },
-  //   },
-  //   interactions: [
-  //     {
-  //       type: 'pointer',
-  //       options: {
-  //         css_selector__root: '.pointerize_root',
-  //         // default_pointer: false,
-  //         start_criteria: {
-  //           criteria: '(pointer: fine)',
-  //           frequency: 'once',
-  //         },
-  //       },
-  //     },
-  //     {
-  //       type: 'pointer__scale',
-  //       options: {
-  //         elements: ['.interaction_target'],
-  //       },
-  //     },
-  //   ],
-  //   // not using `size` because the dimensions stated above need to be tested here
-  //   shapes: [
-  //     // 7 basic shapes
-  //     {
-  //       name: 'circle',
-  //       size: 10,
-  //       guide: [
-  //         {
-  //           type: 'position',
-  //         },
-  //         {
-  //           type: 'pattern',
-  //           options: {
-  //             preset: 'circle',
-  //             area: 'fill',
-  //             ratio: {
-  //               tile: 10,
-  //               gap: {
-  //                 row: 10,
-  //                 column: 10,
-  //               },
-  //             },
-  //           },
-  //         },
-  //       ],
-  //       svg_attributes: {
-  //         'stroke-width': '2',
-  //         stroke: 'red',
-  //         // not using `fill` because a pattern is assigned to it
-  //       },
-  //       animations: [
-  //         {
-  //           preset: 'rotate',
-  //           direction: 'clockwise',
-  //           css_properties: {
-  //             'animation-direction': 'reverse',
-  //           },
-  //         },
-  //       ],
-  //       effects: [
-  //         {
-  //           preset: 'glow',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: 'ellipse',
-  //       size: 10,
-  //       ratio: [
-  //         {
-  //           type: 'radius',
-  //           options: {
-  //             value: 0.4,
-  //           },
-  //         },
-  //       ],
-  //       animations: [
-  //         {
-  //           preset: 'rotate',
-  //           direction: 'anticlockwise',
-  //         },
-  //       ],
-  //       effects: [
-  //         {
-  //           preset: 'glow',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: 'rectangle',
-  //       size: 20,
-  //       svg_attributes: {
-  //         width: '10',
-  //         height: '18',
-  //         transform: 'skewY(10)',
-  //       },
-  //     },
-  //     {
-  //       name: 'polygon',
-  //       size: 30,
-  //       sides: 6,
-  //       ratio: [
-  //         {
-  //           type: 'radius',
-  //           options: {
-  //             type: 'accumulate',
-  //             value: 0.4,
-  //           },
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: 'line',
-  //     },
-  //     {
-  //       name: 'polyline',
-  //     },
-  //     {
-  //       name: 'path',
-  //     },
-  //   ],
-  // }
-  // describe('instance 1', () => {
-  //   const pointerize = new Pointerize(options_1)
-  //   pointerize.start()
+    test('root element', () => {
+      expect(pointerize.element__root).toBe(document.body)
+    })
 
-  //   test('root element', () => {
-  //     expect(pointerize.element__root).toBe(document.body)
-  //   })
+    test('svg element', () => {
+      expect(pointerize.element__svg?.getAttribute('width')).toBe('222')
+      expect(pointerize.element__svg?.style.getPropertyValue('height')).toBe('222px')
+    })
 
-  //   test('svg element', () => {
-  //     expect(pointerize.element__svg?.getAttribute('width')).toBe('222')
-  //     expect(pointerize.element__svg?.style.getPropertyValue('height')).toBe('222px')
-  //   })
+    test('svg container element', () => {
+      expect(pointerize.element__svg_container?.style.getPropertyValue('width')).toBe('333px')
+      expect(pointerize.element__svg_container?.style.getPropertyValue('left')).toBe('10px')
+      expect(pointerize.element__svg_container?.style.getPropertyValue('top')).toBe('100px')
+    })
 
-  //   test('svg container element', () => {
-  //     expect(pointerize.element__svg_container?.style.getPropertyValue('width')).toBe('333px')
-  //     expect(pointerize.element__svg_container?.style.getPropertyValue('left')).toBe('10px')
-  //     expect(pointerize.element__svg_container?.style.getPropertyValue('top')).toBe('100px')
-  //   })
+    test('interactions', () => {
+      //0 pointer interaction
+      //1 check if start criteria passes and pointerize is rendered
+      expect(pointerize.element__svg_container).toBeInstanceOf(HTMLElement)
+      //1 check if no default pointer class is on the root element
+      expect(pointerize.element__root.classList).toContain('-_pointerize__pointer_default__non')
+      //1 check if `pointer__scale` related classes are on target element and it's children
+      expect(document.querySelector<HTMLElement>('.interaction_target')?.dataset.pointerize).toBe('pointer__scale')
+      expect(
+        (<HTMLElement>document.querySelector<HTMLElement>('.interaction_target')?.firstElementChild).dataset.pointerize
+      ).toBe('pointer__scale__child')
+    })
 
-  //   test('interactions', () => {
-  //     //0 pointer interaction
-  //     //1 check if start criteria passes and pointerize is rendered
-  //     expect(pointerize.element__svg_container).toBeInstanceOf(HTMLElement)
-  //     //1 check if no default pointer class is on the root element
-  //     expect(pointerize.element__root.classList).toContain('-_pointerize__pointer_default__non')
-  //     //1 check if `pointer__scale` related classes are on target element and it's children
-  //     expect(document.querySelector<HTMLElement>('.interaction_target')?.dataset.pointerize).toBe('pointer__scale')
-  //     expect(
-  //       (<HTMLElement>document.querySelector<HTMLElement>('.interaction_target')?.firstElementChild).dataset.pointerize
-  //     ).toBe('pointer__scale__child')
-  //   })
-
-  //   test('shapes', () => {
-  //     // the existence of elements and in right order
-  //     expect(document.querySelector('#-_pointerize__container_1th > svg > circle')).not.toBe(null)
-  //     expect(document.querySelector('#-_pointerize__container_1th > svg > path')).not.toBe(null)
-  //     // svg attributes
-  //     expect(document.querySelector('#-_pointerize__container_1th > svg > circle')?.getAttribute('stroke')).toBe('red')
-  //     // animations
-  //     expect(
-  //       document
-  //         .querySelector('#-_pointerize__container_1th > svg > style')
-  //         ?.textContent?.match(/#shape_0th_circle\{[^{]*?animation-direction:reverse[^}]*?\}/)
-  //     )
-  //     expect(
-  //       document
-  //         .querySelector('#-_pointerize__container_1th > svg > style')
-  //         ?.textContent?.match(/#shape_1th_ellipse\{[^{]*?animation-name:rotate[^}]*?\}/)
-  //     )
-  //     // effects
-  //     expect(document.querySelectorAll('#-_pointerize__container_1th > svg > filter')[0].getAttribute('id')).toBe(
-  //       '-_1th__filter_glow_1th_0th'
-  //     )
-  //     expect(document.querySelectorAll('#-_pointerize__container_1th > svg > filter')[1].getAttribute('id')).toBe(
-  //       '-_1th__filter_glow_0th_0th'
-  //     )
-  //   })
-  // })
+    test('shapes', () => {
+      // the existence of elements and in right order
+      expect(document.querySelector('#-_pointerize__container_1th > svg > circle')).not.toBe(null)
+      expect(document.querySelector('#-_pointerize__container_1th > svg > path')).not.toBe(null)
+      // svg attributes
+      expect(document.querySelector('#-_pointerize__container_1th > svg > circle')?.getAttribute('stroke')).toBe('red')
+      // animations
+      expect(
+        document
+          .querySelector('#-_pointerize__container_1th > svg > style')
+          ?.textContent?.match(/#shape_0th_circle\{[^{]*?animation-direction:reverse[^}]*?\}/)
+      )
+      expect(
+        document
+          .querySelector('#-_pointerize__container_1th > svg > style')
+          ?.textContent?.match(/#shape_1th_ellipse\{[^{]*?animation-name:rotate[^}]*?\}/)
+      )
+      // effects
+      expect(document.querySelectorAll('#-_pointerize__container_1th > svg > filter')[0].getAttribute('id')).toBe(
+        '-_1th__filter_glow_1th_0th'
+      )
+      expect(document.querySelectorAll('#-_pointerize__container_1th > svg > filter')[1].getAttribute('id')).toBe(
+        '-_1th__filter_glow_0th_0th'
+      )
+    })
+  })
 
   // // instance 2
   // const options_2: IOptions = {
