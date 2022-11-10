92fe856ae17cb385dc30d25ae0aacaedb30d0a2c
Fatur <afaturechi@gmail.com>
2022-11-10 19:44:18 +0900

    chore: change color

--- lib/brand/tokens.ts
+++ lib/brand/tokens.ts
@@ -2,7 +2,7 @@ const tokens = {
   colors: {
     color_brand_1_2: 'hsl(30, 100%, 40%)', // orange
     color_brand_2_2: 'hsl(70, 100%, 40%)', // yellow-green
-    color_gray_2: 'hsl(0, 0%, 40%)',
+    color_gray_2: 'hsl(0, 0%, 50%)',
   },
 }
 
