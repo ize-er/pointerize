94041910bb5d1fa78ac1a2891e6b9773ac29e1e9
Fatur <afaturechi@gmail.com>
2022-11-10 20:04:33 +0900

    feat: more config options

--- dist__site/tokens.js
+++ dist__site/tokens.js
@@ -2,7 +2,7 @@ const o = {
   colors: {
     color_brand_1_2: "hsl(30, 100%, 40%)",
     color_brand_2_2: "hsl(70, 100%, 40%)",
-    color_gray_2: "hsl(0, 0%, 40%)"
+    color_gray_2: "hsl(0, 0%, 50%)"
   }
 };
 export {
